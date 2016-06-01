ad7a79edb928b195623c8eae1c4f884b5a363f35
redplane <redplane_dt@yahoo.com.vn>
2016-06-01 14:49:25 +0700

    - Initial commit.

---
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using DotnetSignalR.Repository;
+using Shared.Interfaces;
+
+namespace DotnetSignalR.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IRepositoryAccount _repositoryAccount;
+
+        public AdminController(RepositoryAccount repositoryAccount)
+        {
+            _repositoryAccount = repositoryAccount;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        
+        public void Get()
+        {
+        }
+    }
+}
