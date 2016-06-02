e215fab209cb8ef7b89ba392706c0e83e36967d6
redplane <redplane_dt@yahoo.com.vn>
2016-06-02 20:48:24 +0700

    - Reformatted code.

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/IdentityCard.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/IdentityCard.cs
@@ -3,22 +3,22 @@
     public class IdentityCard
     {
         /// <summary>
-        /// Identity card.
+        ///     Identity card.
         /// </summary>
-        public string Id { get; set; } 
+        public string Id { get; set; }
 
         /// <summary>
-        /// Date when identity card was issued.
+        ///     Date when identity card was issued.
         /// </summary>
         public long IssuedDate { get; set; }
 
         /// <summary>
-        /// Place where identity card was issued.
+        ///     Place where identity card was issued.
         /// </summary>
         public Coordinate IssuedPlace { get; set; }
 
         /// <summary>
-        /// Whether identity card is treated as Passport.
+        ///     Whether identity card is treated as Passport.
         /// </summary>
         public bool IsPassport { get; set; }
     }
