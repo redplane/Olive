342642dcc15d0d2fb2274fb9c0f69b82f27bf764
redplane <redplane_dt@yahoo.com.vn>
2016-06-02 21:00:52 +0700

    - Reformatted code.

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/Prescription.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/Prescription.cs
@@ -3,28 +3,28 @@
     public class Prescription
     {
         /// <summary>
-        /// Prescription GUID.
+        ///     Prescription GUID.
         /// </summary>
         public string Id { get; set; }
-        
+
         /// <summary>
-        /// Name of prescription.
+        ///     Name of prescription.
         /// </summary>
         public string Name { get; set; }
-        
+
         /// <summary>
-        /// Valid time.
+        ///     Valid time.
         /// </summary>
         public long From { get; set; }
-        
+
         /// <summary>
-        /// Time before which prescription had been valid.
+        ///     Time before which prescription had been valid.
         /// </summary>
         public long To { get; set; }
-        
+
         /// <summary>
-        /// Medicines patient needs to take.
+        ///     Medicines patient needs to take.
         /// </summary>
-        public Medicine[] Medicines { get; set; } 
+        public Medicine[] Medicines { get; set; }
     }
 }
