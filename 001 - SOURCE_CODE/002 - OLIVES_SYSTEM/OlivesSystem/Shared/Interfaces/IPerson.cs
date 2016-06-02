106c2836b6aaca4263d33878d98c68bae5bf7f22
redplane <redplane_dt@yahoo.com.vn>
2016-06-02 21:07:00 +0700

    - Initial commit.

---
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Interfaces/IPerson.cs
@@ -0,0 +1,67 @@
+﻿using Shared.Models;
+
+namespace Shared.Interfaces
+{
+    public interface IPerson
+    {
+        /// <summary>
+        ///     Person GUID.
+        /// </summary>
+        string Id { get; set; }
+
+        /// <summary>
+        ///     Person last name.
+        /// </summary>
+        string LastName { get; set; }
+
+        /// <summary>
+        ///     Person first name.
+        /// </summary>
+        string FirstName { get; set; }
+
+        /// <summary>
+        ///     Birthday (ticks).
+        /// </summary>
+        long Birthday { get; set; }
+
+        /// <summary>
+        ///     Person gender.
+        /// </summary>
+        byte Gender { get; set; }
+
+        /// <summary>
+        ///     Place where person is living at.
+        /// </summary>
+        Coordinate Address { get; set; }
+
+        /// <summary>
+        ///     Email address which is used for registration or for contacting.
+        /// </summary>
+        string Email { get; set; }
+
+        /// <summary>
+        ///     Password of account.
+        /// </summary>
+        string Password { get; set; }
+
+        /// <summary>
+        ///     Phone number which is used for contacting.
+        /// </summary>
+        string Phone { get; set; }
+
+        /// <summary>
+        ///     Money in wallet.
+        /// </summary>
+        double Money { get; set; }
+
+        /// <summary>
+        ///     Time when account has been registered.
+        /// </summary>
+        long Created { get; set; }
+
+        /// <summary>
+        ///     Role of person [0 - Admin | 1 - Patient | 2 - Doctor]
+        /// </summary>
+        byte Role { get; set; }
+    }
+}
---
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Interfaces/IRepositoryAccount.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shared.Models.Nodes;
+using Shared.ViewModels;
+
+namespace Shared.Interfaces
+{
+    public interface IRepositoryAccount
+    {
+        Task<IPerson> LoginAsync(LoginViewModel info);
+
+        Task<bool> CreatePersonAsync(IPerson info);
+
+        /// <summary>
+        /// Check whether person with specific information exists or not.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        Task<IPerson> GetPersonExistAsync(string email, string password, byte? role);
+
+        /// <summary>
+        /// Filter person by using specific conditions.
+        /// </summary>
+        /// <param name="filter">Specific conditions.</param>
+        /// <returns></returns>
+        Task<IEnumerable<Person>> FilterPersonAsync(FilterPersonViewModel filter);
+
+        /// <summary>
+        /// Filter doctors by using specific conditions.
+        /// </summary>
+        /// <param name="filter">Specific conditions</param>
+        /// <returns></returns>
+        Task<IEnumerable<Doctor>> FilterDoctorAsync(FilterDoctorViewModel filter);
+
+        Task<object> UpdatePersonAsync(string id, IPerson info);
+    }
+}
