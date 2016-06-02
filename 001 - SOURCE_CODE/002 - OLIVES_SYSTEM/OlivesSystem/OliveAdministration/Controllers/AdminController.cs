12b1f6be1b5a8b7f85957e62dfbef605931bb20b
redplane <redplane_dt@yahoo.com.vn>
2016-06-02 17:18:11 +0700

    - Removed index - Added login

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/Controllers/AdminController.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/Controllers/AdminController.cs
@@ -1,27 +1,64 @@
-﻿using System.Web.Mvc;
-using DotnetSignalR.Repository;
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using Shared.Interfaces;
+using Shared.ViewModels;
 
 namespace DotnetSignalR.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController : ParentController
     {
         private readonly IRepositoryAccount _repositoryAccount;
 
-        public AdminController(RepositoryAccount repositoryAccount)
+        /// <summary>
+        ///     Initialize an instance of AdminController.
+        /// </summary>
+        /// <param name="repositoryAccount"></param>
+        public AdminController(IRepositoryAccount repositoryAccount) : base(repositoryAccount)
         {
             _repositoryAccount = repositoryAccount;
         }
 
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Login()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
-        
-        public void Get()
+
+        /// <summary>
+        ///     This function is for posting login information to service.
+        ///     HttpStatusCode != 200 means login is not successful.
+        /// </summary>
+        /// <param name="loginViewModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel loginViewModel)
         {
+            // Response initialization.
+            var response = new ResponseViewModel();
+
+            // Invalid model state.
+            if (!ModelState.IsValid)
+            {
+                response.Errors = RetrieveValidationErrors(ModelState);
+                Response.StatusCode = (int) HttpStatusCode.BadRequest;
+                return Json(response);
+            }
+
+            // Pass parameter to login function. 
+            var result = await _repositoryAccount.LoginAsync(loginViewModel);
+
+            // If no result return, that means no account.
+            if (result == null)
+            {
+                Response.StatusCode = (int) HttpStatusCode.BadRequest;
+                return Json(null);
+            }
+
+            Response.StatusCode = (int) HttpStatusCode.OK;
+            response.Data = result;
+
+            return Json(response);
         }
     }
 }
