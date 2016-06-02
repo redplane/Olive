2859fbd3dfdb82ab115a33f47cfe1a317a7a194b
redplane <redplane_dt@yahoo.com.vn>
2016-06-02 20:47:39 +0700

    - Reformatted code.

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/Doctor.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/Doctor.cs
@@ -6,7 +6,7 @@ namespace Shared.Models.Nodes
     {
         public string Specialization { get; set; }
 
-        public string [] SpecializationAreas { get; set; }
+        public string[] SpecializationAreas { get; set; }
 
         public double Rank { get; set; }
 
