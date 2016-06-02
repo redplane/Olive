3a931b6e13b2770be00e67b4bb5fb120ab474405
redplane <redplane_dt@yahoo.com.vn>
2016-06-02 20:58:01 +0700

    - Re-formatted code.

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/MedicalRecord.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/Shared/Models/Nodes/MedicalRecord.cs
@@ -3,49 +3,49 @@
     public class MedicalRecord
     {
         /// <summary>
-        /// Id of medical record.
-        /// Mã bệnh án.
+        ///     Id of medical record.
+        ///     Mã bệnh án.
         /// </summary>
-        public string Id { get; set; } 
+        public string Id { get; set; }
 
         /// <summary>
-        /// Medical record summary.
-        /// Tổng quan bệnh án.
+        ///     Medical record summary.
+        ///     Tổng quan bệnh án.
         /// </summary>
         public string Summary { get; set; }
 
         /// <summary>
-        /// Examination which is needed doing.
-        /// Các xét nghiệm cần làm.
+        ///     Examination which is needed doing.
+        ///     Các xét nghiệm cần làm.
         /// </summary>
-        public string [] Tests { get; set; }
+        public string[] Tests { get; set; }
 
         /// <summary>
-        /// Diagnoses.
-        /// Các chẩn đoán.
+        ///     Diagnoses.
+        ///     Các chẩn đoán.
         /// </summary>
-        public string [] Diagnoses { get; set; }
+        public string[] Diagnoses { get; set; }
 
         /// <summary>
-        /// Morbidities which goes with the current one.
-        /// Các bệnh liên quan.
+        ///     Morbidities which goes with the current one.
+        ///     Các bệnh liên quan.
         /// </summary>
-        public string [] AdditionalMorbidities { get; set; }
+        public string[] AdditionalMorbidities { get; set; }
 
         /// <summary>
-        /// Differential diagnosis which support to the main diagnose.
-        /// Chẩn đoán phân biệt.
+        ///     Differential diagnosis which support to the main diagnose.
+        ///     Chẩn đoán phân biệt.
         /// </summary>
-        public string [] DifferentialDiagnosis { get; set; }
+        public string[] DifferentialDiagnosis { get; set; }
 
         /// <summary>
-        /// Other pathologies which can affect to the the current diagnose.
+        ///     Other pathologies which can affect to the the current diagnose.
         /// </summary>
-        public string [] OtherPathologies { get; set; }
+        public string[] OtherPathologies { get; set; }
 
         /// <summary>
-        /// GUID of images which about the current medical record.
+        ///     GUID of images which about the current medical record.
         /// </summary>
-        public string [] Images { get; set; }
+        public string[] Images { get; set; }
     }
 }
