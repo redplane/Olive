27b7a7e42d255792fe06d0ea4af4b34818e12efc
redplane <redplane_dt@yahoo.com.vn>
2016-06-03 17:17:37 +0700

    - Moved EditDoctorViewModel.cs and EditDoctorViewModel.cs to OliveAdministration

---
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/ViewModels/EditDoctorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Shared.ViewModels;
+
+namespace DotnetSignalR.ViewModels
+{
+    public class EditDoctorViewModel : EditPersonViewModel
+    {
+        public string Specialization { get; set; }
+
+        public string [] SpecializationAreas { get; set; }
+        
+        public double Rank { get; set; }
+    }
+}
---
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/ViewModels/EditPersonViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Shared.Models;
+
+namespace DotnetSignalR.ViewModels
+{
+    public class EditPersonViewModel
+    {
+        public string Id { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public long Birthday { get; set; }
+
+        public byte Gender { get; set; }
+
+        public Coordinate Address { get; set; }
+        
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public double Money { get; set; }
+
+        public long Created { get; set; }
+
+        public byte Role { get; set; }
+
+        public byte Status { get; set; }
+    }
+}
