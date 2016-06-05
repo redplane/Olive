426cf92f913c001c93894ecc59d8998d83e99d6e
Akai <redplane_dt@yahoo.com.vn>
2016-06-05 23:40:05 +0700

    - Create 2 admins instead of one. - Change localdb to graphenedb

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/Global.asax.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/Global.asax.cs
@@ -85,36 +85,57 @@ namespace DotnetSignalR
             #endregion
 
             #region Admin creation
-
-            var person = new Person();
-            person.FirstName = "Nguyen";
-            person.LastName = "Linh";
-            person.Email = "redplane_dt@yahoo.com.vn";
-            person.Password = "redplane";
-            person.Created = DateTime.Now.Ticks;
-            person.Birthday = DateTime.Now.Ticks;
-            person.Id = "c857a1fe-084c-4926-9ad7-ecfeabd038ff";
-            person.Password = "c857a1fe-084c-4926-9ad7-ecfeabd038ff";
-            person.Role = Roles.Admin;
-            person.Gender = Gender.Male;
-
+            
             // TODO: Remove this code.
-
-            var parameters = new Dictionary<string, object>();
-            parameters.Add("mergeInfo", person);
-            parameters.Add("info", person);
+            var admin = new Person();
+            admin.FirstName = "Nguyen";
+            admin.LastName = "Linh";
+            admin.Email = "redplane_dt@yahoo.com.vn";
+            admin.Password = "redplane";
+            admin.Created = DateTime.Now.Ticks;
+            admin.Birthday = DateTime.Now.Ticks;
+            admin.Id = "2f28d661db9449fdb90d0879f5231fde";
+            admin.Password = "c857a1fe-084c-4926-9ad7-ecfeabd038ff";
+            admin.Role = Roles.Admin;
+            admin.Gender = Gender.Male;
 
             graphClient.Cypher
-                .Merge($"(n:Person {{ Id: '{person.Id}'}})")
+                .Merge($"(n:Person {{ Id: '{admin.Id}'}})")
                 .OnCreate()
                 .Set("n = {info}")
-                .WithParams(parameters)
+                .WithParam("info", admin)
                 .ExecuteWithoutResults();
 
+
+            admin = new Person();
+            admin.FirstName = "Trong";
+            admin.LastName = "Bui";
+            admin.Email = "trong.buiquoc@gmail.com";
+            admin.Password = "redplane";
+            admin.Created = DateTime.Now.Ticks;
+            admin.Birthday = DateTime.Now.Ticks;
+            admin.Id = "adeb011a01a44db08b09dcf0bf2fcd5c";
+            admin.Password = "b453133b7ee466c6dc500ed30b5fd75a";
+            admin.Role = Roles.Admin;
+            admin.Gender = Gender.Male;
+            
+            graphClient.Cypher
+                .Merge($"(n:Person {{ Id: '{admin.Id}'}})")
+                .OnCreate()
+                .Set("n = {info}")
+                .WithParam("info", admin)
+                .ExecuteWithoutResults();
+
+
+            #endregion
+
+            #region Doctors creation
+
+            // TODO: Remove this code.
             var doctorIds = new[]
             {
-                "a43bd26a-f380-45cf-b877-65a6460bc3be", "11b9190a-803b-4610-9a12-0630588de37a",
-                "b14e626e-9e0f-40f8-824e-415c57bdd942", "cdad3953-2994-49d6-8c48-ec3c015b78db"
+                "a43bd26af38045cfb87765a6460bc3be", "11b9190a803b46109a120630588de37a",
+                "b14e626e9e0f40f8824e415c57bdd942", "cdad3953299449d68c48ec3c015b78db"
             };
 
             for (var i = 0; i < doctorIds.Length; i++)
@@ -127,7 +148,7 @@ namespace DotnetSignalR
                 doctor.Created = DateTime.Now.Ticks;
                 doctor.Email = $"Email_{i}";
                 doctor.Created = DateTime.Now.Ticks;
-                doctor.Password = $"Password_{i}";
+                doctor.Password = "FC19B12217DA838C647522D6B6AFC2FC";
                 doctor.Phone = $"00000{i}";
                 doctor.Gender = Gender.Female;
                 doctor.Role = Roles.Doctor;
