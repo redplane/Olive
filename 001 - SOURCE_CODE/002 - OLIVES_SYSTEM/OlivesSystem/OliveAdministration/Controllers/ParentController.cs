6e3462683f049c166a66e5173e18fb3b7d0a410e
redplane <redplane_dt@yahoo.com.vn>
2016-06-07 14:29:08 +0700

    - Added allow header in ParentController - Remove DotnetSignalR to fix page display error.

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/Controllers/ParentController.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesSystem/OliveAdministration/Controllers/ParentController.cs
@@ -46,9 +46,14 @@ namespace OliveAdministration.Controllers
                 // Suppress exception
             }
 
-            HttpContext.Response.AppendHeader("Access-Control-Allow-Origin", "*");
-
+            
             return base.BeginExecuteCore(callback, state);
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            filterContext.HttpContext.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            base.OnActionExecuted(filterContext);
         }
     }
 }
