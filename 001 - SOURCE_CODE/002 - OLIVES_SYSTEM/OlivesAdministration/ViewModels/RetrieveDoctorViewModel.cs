e90b9412c39ef9363bdbb0d94fbb2f62e2109843
redplane <redplane@users.noreply.github.com>
2016-06-09 02:39:19 +0500

    - Removed IdentityCardNo property.

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesAdministration/ViewModels/RetrieveDoctorViewModel.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesAdministration/ViewModels/RetrieveDoctorViewModel.cs
@@ -8,9 +8,5 @@ namespace OlivesAdministration.ViewModels
     {
         [Required(ErrorMessageResourceType = typeof (Language), ErrorMessageResourceName = "RequirePersonalId")]
         public string Id { get; set; }
-
-        [MaxLength(FieldLength.IdentityCardMaxLength, ErrorMessageResourceType = typeof (Language),
-            ErrorMessageResourceName = "InvalidIdentityCard")]
-        public string IdentityCardNo { get; set; }
     }
 }
