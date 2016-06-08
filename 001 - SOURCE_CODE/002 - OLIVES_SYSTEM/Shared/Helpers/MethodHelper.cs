a0f62927cc08cc0572eb83bd5e663e0f2359c92c
redplane <redplane@users.noreply.github.com>
2016-06-09 02:24:28 +0500

    - Added MethodHelper to process function info retrieve.

---
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/Shared/Helpers/MethodHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Shared.Helpers
+{
+    public class MethodHelper
+    {
+        private static MethodHelper _instance;
+
+        public static MethodHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new MethodHelper();
+
+                return _instance;
+            }
+        }
+
+        public string RetrievePropertyName<T>(Expression<Func<T>> propertyLambda)
+        {
+            var property = propertyLambda.Body as MemberExpression;
+
+            if (property == null)
+                throw new ArgumentException("You must pass a lambda of the form: '() => Class.Property' or '() => object.Property'");
+
+            return property.Member.Name;
+        }
+    }
+}
