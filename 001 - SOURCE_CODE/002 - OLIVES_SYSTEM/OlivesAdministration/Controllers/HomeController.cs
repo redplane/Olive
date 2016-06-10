300148b7b474784f84f23d202618eefa6d0c5a0a
Linh Nguyen <redplane_dt@yahoo.com.vn>
2016-06-09 19:34:05 -0700

    - Active both Web Api Route and MVC Route

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesAdministration/Controllers/HomeController.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesAdministration/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ namespace OlivesAdministration.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
+        [Route("signin")]
         public ActionResult Index()
         {
             return View();
