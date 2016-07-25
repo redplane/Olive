9cadf5fee9fb66617c3d66ebef94cf4b7632cf34
Trong Bui <trong.buiquoc@gmail.com>
2016-07-25 20:38:59 +0700

    integrate fontend prototype into Olive

--- 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesAdministration/Controllers/HomeController.cs
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesAdministration/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ namespace OlivesAdministration.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
-        [Route("signin")]
         public ActionResult Index()
         {
             return View();
