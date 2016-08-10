3e4d77c1f57aa0fe5fffe30de8ede77f2c3d25fc
Linh Nguyen <redplane_dt@yahoo.com.vn>
2016-08-11 02:54:18 +0700

    - Implement ModifyMedicalCategory unit test.

---
+++ 001 - SOURCE_CODE/002 - OLIVES_SYSTEM/OlivesAdministrationUnitTest/Controllers/MedicalController/ModifyMedicalCategory.cs
@@ -0,0 +1,155 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OlivesAdministration.Interfaces;
+using OlivesAdministration.Repositories;
+using OlivesAdministration.Test.Helpers;
+using OlivesAdministration.ViewModels.Initialize;
+using Shared.Constants;
+using Shared.Enumerations;
+using Shared.Interfaces;
+using Shared.Models;
+using Shared.Repositories;
+using Shared.Services;
+
+namespace OlivesAdministration.Test.Controllers.MedicalController
+{
+    [TestClass]
+    public class ModifyMedicalCategory
+    {
+        #region Initialization sector
+
+        private OlivesAdministration.Controllers.MedicalCategoryController _medicalController;
+        private IRepositoryMedicalCategory _repositoryMedicalCategory;
+        private ITimeService _timeService;
+        private ILog _log;
+
+        /// <summary>
+        /// Initialize context.
+        /// </summary>
+        private void InitializeContext()
+        {
+            // Data context initialiation.
+            var oliveDataContext = new Repositories.OliveDataContext();
+            
+            // Initialize time service.
+            _timeService = new TimeService();
+
+            // Repositories initialization.
+            _repositoryMedicalCategory = new RepositoryMedicalCategory(oliveDataContext);
+            
+            _log = LogManager.GetLogger(typeof(InitializeMedicalCategory));
+            _medicalController = new OlivesAdministration.Controllers.MedicalCategoryController(_repositoryMedicalCategory, _timeService, _log);
+            EnvironmentHelper.Instance.InitializeController(_medicalController);
+        }
+
+        /// <summary>
+        /// Initialize function context.
+        /// </summary>
+        /// <param name="dataContext"></param>
+        private void InitializeContext(IOliveDataContext dataContext)
+        {
+            _repositoryMedicalCategory = new RepositoryMedicalCategory(dataContext);
+            _log = LogManager.GetLogger(typeof(InitializeMedicalCategory));
+            _timeService = new TimeService();
+            _medicalController = new OlivesAdministration.Controllers.MedicalCategoryController(_repositoryMedicalCategory, _timeService, _log);
+            EnvironmentHelper.Instance.InitializeController(_medicalController);
+        }
+
+        #endregion
+
+        #region Tests
+
+        /// <summary>
+        /// Category name shouldn't be blank
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task MedicalCategoryNameIsBlank()
+        {
+            InitializeContext();
+            var initializer = new InitializeMedicalCategoryViewModel();
+            initializer.Name = null;
+
+            _medicalController.Validate(initializer);
+            var result = await _medicalController.ModifyMedicalCategory(1, initializer);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        /// <summary>
+        /// Category name max length reached.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task MedicalCategoryNameMaxLengthReached()
+        {
+            InitializeContext();
+            var initializer = new InitializeMedicalCategoryViewModel();
+            initializer.Name = "a";
+            for (var i = 0; i < Values.MaxMedicalCategoryNameLength + 10; i++)
+                initializer.Name += "a";
+
+            _medicalController.Validate(initializer);
+            var result = await _medicalController.ModifyMedicalCategory(1, initializer);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        /// <summary>
+        /// Category doesn't exist in database.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task MedicalCategoryDoesntExist()
+        {
+            var dataContext = new Repositories.OliveDataContext();
+            await EnvironmentHelper.Instance.InitializeMedicalCategories(dataContext.Context, 10);
+            InitializeContext(dataContext);    
+
+            var modifier = new InitializeMedicalCategoryViewModel();
+            modifier.Name = "12";
+
+            var result = await _medicalController.ModifyMedicalCategory(11, modifier);
+            Assert.AreEqual(HttpStatusCode.NotFound, result.StatusCode);
+        }
+
+        /// <summary>
+        /// Medical name is already in database.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task MedicalCategoryNameConflict()
+        {
+            var dataContext = new Repositories.OliveDataContext();
+            await EnvironmentHelper.Instance.InitializeMedicalCategories(dataContext.Context, 10);
+            InitializeContext(dataContext);
+
+            var modifier = new InitializeMedicalCategoryViewModel();
+            modifier.Name = "0";
+
+            var result = await _medicalController.ModifyMedicalCategory(1, modifier);
+            Assert.AreEqual(HttpStatusCode.Conflict, result.StatusCode);
+        }
+
+        /// <summary>
+        /// Modification is succesful.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task MedicalCategoryModifySuccessfully()
+        {
+            var dataContext = new Repositories.OliveDataContext();
+            await EnvironmentHelper.Instance.InitializeMedicalCategories(dataContext.Context, 10);
+            InitializeContext(dataContext);
+
+            var modifier = new InitializeMedicalCategoryViewModel();
+            modifier.Name = "10";
+
+            var result = await _medicalController.ModifyMedicalCategory(1, modifier);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+        #endregion
+    }
+}
